06b81bf28c5e70e9c3af43ff7f40ae5187d59ca7
Root <paglipay@gmail.com>
2021-07-22 17:12:33 -0700

    feedback

---
+++ main.py
@@ -0,0 +1,25 @@
+import subprocess
+import shlex
+import time
+
+def run_command(command):
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline().decode("utf-8")
+        if len(output) < 1:
+            break
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+    rc = process.poll()
+    return rc
+
+if __name__ == "__main__":
+    print('hello')
+    run_command('git add .')
+    run_command('git commit -m "feedback"')
+    run_command('git push origin master')
+    #time.sleep(10)
+    # from subprocess import call
+    # call(["git add . && git commit -am 'feedback' && git push origin master"])
