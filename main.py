40564f3d2c8bbd9a807d93d66293ab9d2fbcd58c
Paul Aglipay <you@example.com>
2021-10-15 15:34:06 -0700

    ubd-220

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         'git status',
         'git commit -m "' + socket.gethostname() + '"',
         'git config --global user.email "you@example.com"',
-        'git config --global user.name "Your Name"'
+        'git config --global user.name "Your Name"',
         # 'whoami',
         # 'pwd',
         # 'ls -la',
