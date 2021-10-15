b07d182ff03fb724a9017fc09ffe987f53f0aa26
Paul Aglipay <paglipay@gmail.com>
2021-10-14 18:22:45 -0700

    feedback

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         'whoami',
         'pwd',
         'ls -la',
-        'echo bye >> test.txt',
+        'sed -i \'23iabcd\' test.txt',
         'git add .',
         'git status',
         'git commit -m "feedback"',
