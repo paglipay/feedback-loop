511e79d3a522d84ea63d83f186baba2dc2cfa91b
Paul Aglipay <paglipay@gmail.com>
2021-10-14 19:47:25 -0700

    feedback

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def run_command(command):
 
 if __name__ == "__main__":
     print('hello')
+    log = open('some file.txt', 'a')
+    log.write('some text, as header of the file\n')
+    log.flush()  # <-- here's something not to forget!
+    c = subprocess.Popen(['dir', '/p'], stdout=log, stderr=log, shell=True)
     command_list = [
         'git pull origin master',
         'whoami',
