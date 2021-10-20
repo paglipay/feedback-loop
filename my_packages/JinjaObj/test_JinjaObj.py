fd958488a0b667b95a831cfd13a1e5c167472718
Your Name <you@example.com>
2021-10-20 11:23:35 -0700

    ub-desk-230

--- my_packages/JinjaObj/test_JinjaObj.py
+++ my_packages/JinjaObj/test_JinjaObj.py
@@ -1,4 +1,6 @@
 from my_packages.JinjaObj import JinjaObj
+from DTree import DTree
+import json, yaml
 import unittest   # The test framework
 
 class Test_JinjaObj(unittest.TestCase):
@@ -9,3 +11,16 @@ class Test_JinjaObj(unittest.TestCase):
     def test_v_func(self):
         k = JinjaObj('test_v_func', {})
         self.assertEqual(k.v_func('hi'), True)
+
+    
+    def test_Dtree(self):
+        print('app.py HERE')
+        flask_data = {}
+        flask_process = {}
+        import_obj_instance = {}
+        json_file = 'my_packages/JinjaObj/json/_create_list.json'
+        d = DTree(json.load(open(json_file)), name=json_file, import_obj_instance=import_obj_instance, data=flask_data)
+        self.assertEqual(json.load(open('my_packages/JinjaObj/json/out.json')), d.data['my_packages/JinjaObj/json/out.json'])   
+        self.assertEqual(yaml.safe_load(open('my_packages/JinjaObj/json/out.yml')), d.data['my_packages/JinjaObj/json/out.yml'])    
+    
+
