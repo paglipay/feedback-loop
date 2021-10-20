6376a72f6e3a863a7c9b686270c79991832fbdfa
Your Name <you@example.com>
2021-10-20 10:32:34 -0700

    ubd-220

--- my_packages/Key/test_key.py
+++ my_packages/Key/test_key.py
@@ -1,4 +1,6 @@
 from my_packages.Key import Key
+from DTree import DTree
+import json
 import unittest   # The test framework
 
 class Test_Key(unittest.TestCase):
@@ -10,5 +12,13 @@ class Test_Key(unittest.TestCase):
         k = Key('test_v_func', {})
         self.assertEqual(k.v_func('hi'), True)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_Dtree(self):
+        print('app.py HERE')
+        flask_data = {}
+        flask_process = {}
+        import_obj_instance = {}
+        json_file = 'my_packages/Key/tests/json/start.json'
+        d = DTree(json.load(open(json_file)), name=json_file, import_obj_instance=import_obj_instance, data=flask_data)
+        # print('d.data:', d.data)
+        self.assertEqual(d.data, {'Key': ['Hi Key']})
+
